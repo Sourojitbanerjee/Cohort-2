9cf97acd25aeafd2d1b4c7e76b318f225f0708ce
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-08-28 18:42:21 +0530

    initial commits

---
+++ class_1/arrays.js
@@ -0,0 +1,9 @@
+// const personArray = ["Sourojit", "rishaan", "raman"];
+// console.log(personArray[0])
+
+// const ages = [12, 45, 34, 67, 24, 56, 68];
+// for(let i = 0; i < ages.length; i++){
+//     if(ages[i] % 2 == 0){
+//         console.log(ages[i]);
+//     }
+// }
---
+++ class_1/main.js
@@ -0,0 +1,13 @@
+// console.log("hello world")
+let firstName = "Sourojit";
+let age = 19;
+let isMarried = false;
+
+// console.log("this persons name is " +firstName + " and his age is " +age )
+if (isMarried == true){
+    console.log(firstName + " is married");
+
+}
+else {
+    console.log(firstName + " is not married" )
+}
