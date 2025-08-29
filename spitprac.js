6b3ab7de6b655f6865172624f2883d198bbbd7eb
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-08-30 01:47:45 +0530

    loops

---
+++ spitprac.js
@@ -0,0 +1,24 @@
+// // let ans = 0 ;
+// // for(let i = 1; i <= 10; i++){
+// //     ans = ans + i;
+
+// // }
+// // // console.log(ans);
+
+// // for(let i = 1; i <= 20; i++){
+// //     console.log(i);
+
+// // }
+
+// // for(let i = 10; i >= 1; i--){
+// //     console.log(i);
+// // }
+
+// let ans = 1;
+// while (ans <= 1000) {
+//   ans = ans * 2;
+// }
+// console.log(ans);
+
+
+
