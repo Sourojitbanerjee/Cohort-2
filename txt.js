ceb2ea8882b897bea5bf7b7a5406b323ce36b3ef
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-09-05 02:32:07 +0530

    tests

--- txt.js
+++ txt.js
@@ -9,3 +9,35 @@ function sumofSquares(a, b){
 }
 
 console.log(sumofSquares(2,3));
+
+
+
+function Square(n){
+    return n*n;
+}
+function Cube(n){
+    return n*n*n;
+}
+function Rectangle(n, m){
+    return n*m;
+}
+
+function sumofSquares(a, b){
+    const val1 = Square(a);
+    const val2 = Square(b);
+}
+return val1+val2;
+
+function sumofCubes(a, b){
+    const val1 = Cube(a);
+    const val1 = Cube(b);
+}
+return val1+val2;
+
+function areaofRectangle(l, b){
+    const val1 = length(l);
+    const val2 = breadth(b);
+}
+return val1*val2;
+
+console.log(areaofRectangle(4, 8));
