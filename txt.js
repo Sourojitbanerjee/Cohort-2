11329259386312b4835e5c189340d09ebab5927b
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-09-06 18:43:52 +0530

    minor

--- txt.js
+++ txt.js
@@ -1,43 +1,39 @@
-function Square(n){
-    return n*n;
+function Square(n) {
+  return n * n;
 }
 
-function sumofSquares(a, b){
-    const val1 = Square(a);
-    const val2 = Square(b);
-    return val1 + val2;
+function sumofSquares(a, b) {
+  const val1 = Square(a);
+  const val2 = Square(b);
+  return val1 + val2;
 }
 
-console.log(sumofSquares(2,3));
+console.log(sumofSquares(2, 3));
 
-
-
-function Square(n){
-    return n*n;
+function Square(n) {
+  return n * n;
 }
-function Cube(n){
-    return n*n*n;
+function Cube(n) {
+  return n * n * n;
 }
-function Rectangle(n, m){
-    return n*m;
+function Rectangle(n, m) {
+  return n * m;
 }
 
-function sumofSquares(a, b){
-    const val1 = Square(a);
-    const val2 = Square(b);
+function sumofSquares(a, b) {
+  const val1 = Square(a);
+  const val2 = Square(b);
+  return val1 + val2;
 }
-return val1+val2;
 
-function sumofCubes(a, b){
-    const val1 = Cube(a);
-    const val2 = Cube(b);
+function sumofCubes(a, b) {
+  const val1 = Cube(a);
+  const val2 = Cube(b);
+  return val1 + val2;
 }
-return val1+val2;
 
-function areaofRectangle(l, b){
-    const val1 = length(l);
-    const val2 = breadth(b);
+function areaofRectangle(l, b) {
+  return l * b;
 }
-return val1*val2;
 
 console.log(areaofRectangle(4, 8));
