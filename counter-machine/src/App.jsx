1be3cb272759c8d1ca97d91d2228382e1760c92a
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-11-09 04:16:28 +0530

    added count app

--- counter-machine/src/App.jsx
+++ counter-machine/src/App.jsx
@@ -1,12 +1,17 @@
-
-import './App.css'
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function onClickHandler() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
-    hi there!
+      <button onClick={onClickHandler}>Counter {count}</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
